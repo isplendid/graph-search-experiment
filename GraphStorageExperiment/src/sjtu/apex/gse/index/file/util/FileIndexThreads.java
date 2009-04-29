07c2b5bf5fefe4e256ecb190033c452c476732e8
fieldfar <fieldfar@gmail.com>
2009-04-29 12:13:28 +0000

    - Indexing part reconstructed.

---
+++ GraphStorageExperiment/src/sjtu/apex/gse/index/file/util/FileIndexThreads.java
@@ -0,0 +1,133 @@
+package sjtu.apex.gse.index.file.util;
+
+import sjtu.apex.gse.filesystem.FilesystemUtility;
+import sjtu.apex.gse.index.file.FileIndexWriter;
+import sjtu.apex.gse.storage.file.FileRepositoryWriter;
+import sjtu.apex.gse.storage.file.RID;
+import sjtu.apex.gse.storage.file.RecordRange;
+import sjtu.apex.gse.temp.file.TempFileEntry;
+import sjtu.apex.gse.temp.file.TempRepositoryFileReader;
+import sjtu.apex.gse.temp.file.TempRepositoryFileWriter;
+import sjtu.apex.gse.temp.file.util.TempRepositorySorter;
+
+/**
+ * This class manages 
+ * 
+ * @author Tian Yuan
+ *
+ */
+public class FileIndexThreads {
+
+	private TempRepositoryFileWriter tw;
+	private String dtfldr, trfn, tsfn, tmpfldr, strgfn, idxfn;
+	private int ec, mec, mtc, tc, size;
+
+	public FileIndexThreads(int size, int maxEntryCount, int maxThreadCount, String tempFolder, String dataFolder) {
+		this.size = size;
+		dtfldr = dataFolder;
+		trfn = tempFolder + "/raw" + (size - 1);
+		tsfn = tempFolder + "/sort" + (size - 1);
+		tmpfldr = tempFolder + "/SortTmp";
+		strgfn = dataFolder + "/storage" + (size - 1);
+		idxfn = dataFolder + "/index" + (size - 1);
+		tw = new TempRepositoryFileWriter(trfn, size);
+		ec = 0;
+		tc = 0;
+		mec = maxEntryCount;
+		mtc = maxThreadCount;
+	}
+
+	public void addEntry(String pattern, int[] ins) {
+		tw.writeRecord(new TempFileEntry(pattern, ins));
+		ec++;
+
+		if (ec > mec) buildIndex();
+	}
+
+	public void close() {
+		buildIndex();
+		mergeIndex();
+
+		if (FilesystemUtility.fileExist(idxfn) && FilesystemUtility.fileExist(strgfn)) {
+			FilesystemUtility.renameFile(idxfn, idxfn  + ".t1");
+			FilesystemUtility.renameFile(strgfn, strgfn  + ".t1");       
+			FileIndexMerger.merge(dtfldr, dtfldr, idxfn, strgfn, size, 2);
+	        for (int i = 0; i < 2; i++) {
+	            FilesystemUtility.deleteFile(idxfn + ".t" + i);
+	            FilesystemUtility.deleteFile(strgfn + ".t" + i);
+	        }
+		} else {
+			FilesystemUtility.renameFile(idxfn + ".t0", idxfn);
+			FilesystemUtility.renameFile(strgfn + ".t0", strgfn);
+		}
+	}
+
+	/**
+	 * Build index for the temporary files into a thread
+	 */
+	private void buildIndex() {
+		tw.close();
+		TempRepositorySorter.sort(trfn, tsfn, size, tmpfldr);
+		FilesystemUtility.deleteFile(trfn);
+		index(tsfn, strgfn + ".t" + tc, idxfn + ".t" + tc, 2);
+		FilesystemUtility.deleteFile(tsfn);
+		tc++;
+
+		if (tc > mtc)
+			mergeIndex();
+	}
+
+	/**
+	 * Merge all threads into a single index
+	 */
+	private void mergeIndex() {
+		if (tc == 1)
+			return;
+		FileIndexMerger.merge(dtfldr, dtfldr, idxfn + ".t", strgfn + ".t", size, tc);
+		for (int i = 0; i < tc; i++) {
+			FilesystemUtility.deleteFile(idxfn + ".t"  + i);
+			FilesystemUtility.deleteFile(strgfn + ".t" + i);
+		}
+		FilesystemUtility.renameFile(idxfn + ".t", idxfn + ".t" + "0");
+		FilesystemUtility.renameFile(strgfn + ".t", strgfn + ".t" + "0");
+		tc = 1;
+	}
+
+	/**
+	 * This method takes in a sorted temp repository file and generate
+	 * a repository file and its index
+	 * @param src
+	 * @param dest
+	 * @param idx 
+	 * @param size
+	 */
+	static private void index(String src, String dest, String idx, int size) {
+		TempRepositoryFileReader srd = new TempRepositoryFileReader(src, size);
+		FileRepositoryWriter dwr = new FileRepositoryWriter(dest, size);
+		FileIndexWriter iwr = new FileIndexWriter(idx, size);
+		TempFileEntry tfe;
+		RID start = null, last = null;
+		String currentPat = null;
+
+		while ((tfe = srd.readRecord()) != null) {
+			if (currentPat == null) {
+				start = dwr.getRID();
+				currentPat = tfe.pattern;
+			}
+			else if (!currentPat.equals(tfe.pattern)) {
+				iwr.writeEntry(currentPat, new RecordRange(start, last));
+				start = dwr.getRID();
+				currentPat = tfe.pattern;
+			}
+
+			last = dwr.getRID();
+			dwr.writeEntry(tfe.ins);
+		}
+
+		if (currentPat != null) iwr.writeEntry(currentPat, new RecordRange(start, last));
+
+		srd.close();
+		dwr.close();
+		iwr.close();
+	}
+}
--- GraphStorageExperiment/src/sjtu/apex/gse/indexer/InstanceKeywordRepository.java
+++ GraphStorageExperiment/src/sjtu/apex/gse/indexer/InstanceKeywordRepository.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
 
 /**
  * This class reads keyword information from a file
