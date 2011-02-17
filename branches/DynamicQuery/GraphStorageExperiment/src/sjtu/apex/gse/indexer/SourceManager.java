7fd6883f75f46cbe3f76c14085da86eaa1c61386
fieldfar <fieldfar@0fcec150-9b2c-11dd-93bb-3f8bb607bf45>
2011-02-17 11:27:37 +0000

    + Trivial modifications to the IDManager's comment + New SourceManager interface

---
+++ branches/DynamicQuery/GraphStorageExperiment/src/sjtu/apex/gse/indexer/SourceManager.java
@@ -0,0 +1,36 @@
+package sjtu.apex.gse.indexer;
+
+public interface SourceManager {
+	
+    /**
+     * Initialize the index storage, to create the necessary tables
+     */
+    public void install();
+    
+    /** 
+     * Return the uri of the source given the id
+     * @param id - The id of the source
+     * @return The uri of the source
+     */
+    public String getSource(int id);
+    
+    /**
+     * Return the id of the source given the uri
+     * @param source - The uri of the source
+     * @return The id of the resource. If the URI does not exist in the dictionary, return -1.
+     */
+    public int getID(String source);
+    
+    /**
+     * Add a new uri using a subsequent id
+     * @param uri - The uri to the source to be added
+     * @return The id of the new uri
+     */
+    public int addSource(String uri);
+    
+    /**
+     * Release all resources occupied by this manager
+     */
+    public void close();
+
+}
