f292f9e3b8063e3a0eea10ec184ee5a2074eae47
fieldfar <fieldfar@0fcec150-9b2c-11dd-93bb-3f8bb607bf45>
2011-03-08 12:21:19 +0000

    + Created modes in the query system

--- branches/DynamicQuery/GraphStorageExperiment/src/sjtu/apex/gse/system/QuerySystem.java
+++ branches/DynamicQuery/GraphStorageExperiment/src/sjtu/apex/gse/system/QuerySystem.java
@@ -3,14 +3,23 @@ package sjtu.apex.gse.system;
 import sjtu.apex.gse.config.Configuration;
 import sjtu.apex.gse.hash.HashFunction;
 import sjtu.apex.gse.hash.ModHash;
+import sjtu.apex.gse.indexer.IDManager;
+import sjtu.apex.gse.indexer.SourceManager;
+import sjtu.apex.gse.indexer.file.SleepyCatIDManager;
+import sjtu.apex.gse.indexer.file.SleepyCatSourceManager;
 import sjtu.apex.gse.metadata.IndexManager;
 import sjtu.apex.gse.metadata.PatternManager;
+import sjtu.apex.gse.operator.factory.FileOperatorFactory;
+import sjtu.apex.gse.operator.factory.OperatorFactory;
+import sjtu.apex.gse.operator.factory.WebOperatorFactory;
+import sjtu.apex.gse.pattern.DirectPatternCodec;
 import sjtu.apex.gse.pattern.HashingPatternCodec;
 import sjtu.apex.gse.pattern.PatternCodec;
 import sjtu.apex.gse.planner.Planner;
 import sjtu.apex.gse.planner.dp.DynamicProgrammingPlanner;
 import sjtu.apex.gse.storage.map.ColumnNodeMap;
 import sjtu.apex.gse.storage.map.HashLexicoColumnNodeMap;
+import sjtu.apex.gse.storage.map.LexicoColumnNodeMap;
 
 
 public class QuerySystem {
@@ -23,20 +32,74 @@ public class QuerySystem {
 	private String folder;
 	private int pl, psl;
 	
+	private SourceManager sourceMan;
+	private ie.deri.urq.lidaq.repos.WebRepository webRepos;
+	private IDManager idMan;
+	
+	public enum QuerySystemMode {
+		WEB_ONLY, FILE_ONLY 
+	}
+	
 	public QuerySystem(Configuration config) {
-		HashFunction hf;
+		this(config, QuerySystemMode.WEB_ONLY);
+	}
+	
+	public QuerySystem(Configuration config, QuerySystemMode mode) {
 		this.config = config;
-		codec = new HashingPatternCodec(new ModHash(config));
 		
-		folder = config.getStringSetting("DataFolder", null);
-		pl = config.getIntegerSetting("PatternLength", 0);
-		psl = config.getIntegerSetting("PatternStrSize", 128);
+		paramsInit();
 		
 		indexMan = new IndexManager(folder, pl, psl);
-		hf = new ModHash(config);
-		patternMan = new PatternManager(codec, indexMan, hf);
-		columnNodeMap = new HashLexicoColumnNodeMap(hf);
-		queryPlanner = new DynamicProgrammingPlanner(this);
+		
+		
+		patternManagerInit();
+		queryPlannerInit(mode);
+		
+		
+		sourceMan = new SleepyCatSourceManager(config);
+		idMan = new SleepyCatIDManager(config);
+		webRepos = new ie.deri.urq.lidaq.repos.WebRepository(null, null);
+	}
+	
+	private void paramsInit() {
+		folder = config.getStringSetting("DataFolder", null);
+		
+		pl = config.getIntegerSetting("PatternLength", 0);
+		psl = config.getIntegerSetting("PatternStrSize", 128);
+	}
+	
+	private void patternManagerInit() {
+		String pt = config.getStringSetting("PatternType", "Hash").toLowerCase();
+		
+		if (pt.equals("hash")) {
+			HashFunction hf = new ModHash(config);
+			codec = new HashingPatternCodec(hf);
+			columnNodeMap = new HashLexicoColumnNodeMap(hf);
+		} else if (pt.equals("direct")){
+			codec = new DirectPatternCodec(); 
+			columnNodeMap = new LexicoColumnNodeMap();
+		}
+		
+		patternMan = new PatternManager(codec, indexMan);
+	}
+	
+	private void queryPlannerInit(QuerySystemMode mode) {
+		OperatorFactory opFac = null;
+		
+		switch (mode) {
+			case WEB_ONLY:
+				opFac = new WebOperatorFactory();
+				break;
+			case FILE_ONLY:
+				opFac = new FileOperatorFactory();
+				break;
+		}
+	
+		queryPlanner = new DynamicProgrammingPlanner(this, opFac);
+	}
+	
+	public ie.deri.urq.lidaq.repos.WebRepository webRepository() {
+		return webRepos;
 	}
 	
 	public PatternManager patternManager() {
@@ -49,6 +112,14 @@ public class QuerySystem {
 	
 	public IndexManager indexManager() {
 		return indexMan;
+	}
+	
+	public SourceManager sourceManager() {
+		return sourceMan;
+	}
+	
+	public IDManager idManager() {
+		return idMan;
 	}
 	
 	public ColumnNodeMap columnNodeMap() {
@@ -73,6 +144,12 @@ public class QuerySystem {
 	
 	public int patternStrSize() {
 		return psl;
+	}
+	
+	public void close() {
+		sourceMan.close();
+		idMan.close();
+		webRepos.shutdown();
 	}
 	
 }
