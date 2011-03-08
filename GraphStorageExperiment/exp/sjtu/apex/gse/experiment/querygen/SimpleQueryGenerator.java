0f52f5b1f01c92202e6306b56f8475735ea5ea63
fieldfar <fieldfar@0fcec150-9b2c-11dd-93bb-3f8bb607bf45>
2011-03-08 12:27:38 +0000

    + Changes to adapt to the new system

--- GraphStorageExperiment/exp/sjtu/apex/gse/experiment/querygen/SimpleQueryGenerator.java
+++ GraphStorageExperiment/exp/sjtu/apex/gse/experiment/querygen/SimpleQueryGenerator.java
@@ -10,9 +10,7 @@ import java.util.Map;
 import sjtu.apex.gse.config.Configuration;
 import sjtu.apex.gse.config.FileConfig;
 import sjtu.apex.gse.indexer.IDManager;
-import sjtu.apex.gse.indexer.LabelManager;
-import sjtu.apex.gse.indexer.file.SleepyCatIDManager;
-import sjtu.apex.gse.indexer.file.SleepyCatLabelManager;
+import sjtu.apex.gse.storage.file.FileRepositoryEntry;
 import sjtu.apex.gse.storage.file.FileRepositoryReader;
 import sjtu.apex.gse.storage.file.RecordRange;
 import sjtu.apex.gse.struct.QueryGraph;
@@ -32,69 +30,67 @@ public class SimpleQueryGenerator {
 	static double base = 0.2;
 
 	/**
-	 * @param args
+	 * @param args A list of filenames which in order represent the config file for the system, edge candidate, 
+	 * output file and the config file for the query generator.
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
-		Configuration config = new FileConfig(args[3]);
-		prob = config.getDoubleSetting("SimpleGenExt", 0.0001);
-		base = config.getDoubleSetting("SimpleGenBase", 0.2);
-		QuerySystem sys = new QuerySystem(new FileConfig(args[0]));
+		Configuration config = new FileConfig(args[0]);
+		prob = config.getDoubleSetting("SimpleGenExt", prob);
+		base = config.getDoubleSetting("SimpleGenBase", base);
+		QuerySystem sys = new QuerySystem(config);
 		BufferedReader rd = new BufferedReader(new FileReader(args[1]));
 		
 		String dataFolder = sys.config().getStringSetting("DataFolder", null);
-		IDManager idman = new SleepyCatIDManager(sys.config());
-		LabelManager labman = new SleepyCatLabelManager(sys.config());
+		IDManager idman = sys.idManager();
+//		LabelManager labman = new SleepyCatLabelManager(sys.config());
 		String temp;
 		int count = 0;
 		
 		while ((temp = rd.readLine()) != null) {
 			if (temp.length() == 0) continue;
 			String p = temp.split("\t")[0];
-			RecordRange rr = sys.indexManager().seek("*[+" + p + "::*]", 2);
+			RecordRange rr = sys.indexManager().seek("*[+" + Integer.toString(idman.getID(p)) + "::*]", 2);
 
 			FileRepositoryReader fr = new FileRepositoryReader(dataFolder + "/storage1", 2, rr);
-			QueryGraph g = sys.patternCodec().decodePattern("*[+" + p + "::*]");
+			QueryGraph g = sys.patternCodec().decodePattern("*[+" + Integer.toString(idman.getID(p)) + "::*]");
 			Map<QueryGraphNode, Integer> map = sys.columnNodeMap().getMap(g);
 			QueryGraphEdge edge = g.getEdge(0);
-			int ent[];
+			FileRepositoryEntry ent;
 
 			BufferedWriter wr = null;
 			while ((ent = fr.readEntry()) != null) {
+				if (Math.random() < (prob + (wr == null? base : 0))) {
+					int ss = ent.bindings[map.get(edge.getNodeFrom())];
+					int os = ent.bindings[map.get(edge.getNodeTo())];
+					String sub, obj;
+					String pred = idman.getURI(edge.getLabel());
+					
+					if (Math.random() < 0.5) {
+						sub = idman.getURI(ss);
+						obj = "*";
+					}
+					else {
+						sub = "*";
+						obj = idman.getURI(os);
+					}
+					
+					if (wr == null) {
+						wr = new BufferedWriter(new FileWriter(args[2] + "." + count));
+						count++;
+					}
+					wr.append("2 1\n");
+					wr.append(sub + "\n");
+					wr.append(obj + "\n");
+					wr.append("1 2 " + pred + "\n");
 
-				int ss = ent[map.get(edge.getNodeFrom())];
-				int os = ent[map.get(edge.getNodeTo())];
-				String sub = idman.getURI(ss);
-				String pred = edge.getLabel();
-				String obj = idman.getURI(os);
-
-				String[] left = labman.getLabel(sub);
-				String[] right = labman.getLabel(obj);
-
-				if (left == null || right == null) {
-					continue;
 				}
-
-
-				for (String l : left) {
-					for (String r : right) 
-						if (!l.equals(r) && Math.random() < (prob + (wr == null? base : 0))){
-							if (wr == null) {
-								wr = new BufferedWriter(new FileWriter(args[2] + "." + count));
-								count++;
-							}
-							wr.append("2 1\n");
-							wr.append(l + "\n");
-							wr.append(r + "\n");
-							wr.append("1 2 " + pred + "\n");
-						}
-				}
-
-			}
+			}	
 			if (wr != null) wr.close();
 		}			
 		
 		rd.close();
+		sys.close();
 	}
 
 }
