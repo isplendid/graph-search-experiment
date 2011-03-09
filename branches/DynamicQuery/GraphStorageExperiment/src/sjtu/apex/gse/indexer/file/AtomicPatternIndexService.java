dffc6cdd8808a30195fe8a0cab348df5530f9af2
fieldfar <fieldfar@0fcec150-9b2c-11dd-93bb-3f8bb607bf45>
2011-03-09 18:56:23 +0000

    + Atomic indexer changed to support new data format

--- branches/DynamicQuery/GraphStorageExperiment/src/sjtu/apex/gse/indexer/file/AtomicPatternIndexService.java
+++ branches/DynamicQuery/GraphStorageExperiment/src/sjtu/apex/gse/indexer/file/AtomicPatternIndexService.java
@@ -1,8 +1,17 @@
 package sjtu.apex.gse.indexer.file;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
+import java.util.zip.GZIPInputStream;
+
+import org.semanticweb.yars.nx.parser.NxParser;
+import org.semanticweb.yars.nx.parser.ParseException;
 
 import sjtu.apex.gse.config.Configuration;
 import sjtu.apex.gse.config.FileConfig;
@@ -10,12 +19,13 @@ import sjtu.apex.gse.hash.HashFunction;
 import sjtu.apex.gse.hash.ModHash;
 import sjtu.apex.gse.index.file.util.FileIndexer;
 import sjtu.apex.gse.indexer.IDManager;
-import sjtu.apex.gse.indexer.InstanceKeywordRepository;
-import sjtu.apex.gse.indexer.RelationRepository;
+import sjtu.apex.gse.indexer.SourceManager;
+import sjtu.apex.gse.pattern.DirectPatternCodec;
 import sjtu.apex.gse.pattern.HashingPatternCodec;
 import sjtu.apex.gse.pattern.PatternCodec;
 import sjtu.apex.gse.storage.map.ColumnNodeMap;
 import sjtu.apex.gse.storage.map.HashLexicoColumnNodeMap;
+import sjtu.apex.gse.storage.map.LexicoColumnNodeMap;
 import sjtu.apex.gse.struct.QueryGraph;
 import sjtu.apex.gse.struct.QueryGraphNode;
 
@@ -24,108 +34,127 @@ public class AtomicPatternIndexService {
 	private int maxSize;
 	private FileIndexer fidx;
 	private IDManager idman;
-//	private LabelManager lblman;
+	private SourceManager srcman;
 	private PatternCodec codec;
 	private ColumnNodeMap cnm;
-//	private Configuration config;
-	private HashFunction hash;
 	
 	public AtomicPatternIndexService(Configuration config) {
 		maxSize = config.getIntegerSetting("PatternLength", 3);
 		fidx = new FileIndexer(maxSize, config);
 		idman = new SleepyCatIDManager(config);
-//		lblman = new SleepyCatLabelManager(config);
-		hash = new ModHash(config);
-		codec = new HashingPatternCodec(hash);
-		cnm = new HashLexicoColumnNodeMap(hash);
-//		this.config = config;
-	}
-	
-	private void indexLabel(String filename) {
-		InstanceKeywordRepository rd = new InstanceKeywordRepository(filename);
-		int cnt = 0;
+		srcman = new SleepyCatSourceManager(config);
 		
-		while(rd.next()) {
-			cnt++;
-			if(cnt % 1000 == 0)
-				System.out.println("node " + cnt);
-			
-			String uri = rd.getInstance();
-			String[] terms = rd.getWordList();
-			
-			int id = idman.getID(uri);
-			if(id == -1)
-				id = idman.addURI(uri);
-			
-			for(int i = 0; i < terms.length; i++) {
-				int[] ins = new int[1];
-				ins[0] = id;
-				fidx.addEntry(terms[i], 1, ins);
-			}
-		}
-		rd.close();
-	}
-	
-	private void indexEdge(String filename) {
-		RelationRepository rd = new RelationRepository(filename);
-		int count = 0;
+		String pt = config.getStringSetting("PatternType", "Hash").toLowerCase();
 		
-		while (rd.next()) {
-			String sub = rd.getSubject();
-			String pred = rd.getPredicate();
-			String obj = rd.getObject();
-			
-
-			int ss = idman.getID(sub);
-			int os = idman.getID(obj);
-			
-			if (ss == -1 || os == -1) continue;
-
-			if ((++count) % 1000 == 0) System.out.println(count);
-
-			QueryGraph g = new QueryGraph();
-			QueryGraphNode snode = g.addNode();
-			QueryGraphNode onode = g.addNode();
-
-			g.addEdge(snode, onode, pred);
-			
-			Map<QueryGraphNode, Integer> map = new HashMap<QueryGraphNode, Integer>();
-			map.put(snode, ss);
-			map.put(onode, os);
-
-			addPattern(g, map);
-			
+		if (pt.equals("hash")) {
+			HashFunction hash = new ModHash(config);
+			codec = new HashingPatternCodec(hash);
+			cnm = new HashLexicoColumnNodeMap(hash);
+		} else if (pt.equals("direct")) {
+			codec = new DirectPatternCodec(); 
+			cnm = new LexicoColumnNodeMap();
 		}
-		rd.close();
 	}
 	
-	public void indexAtomicPatterns(String labelFile, String relationFile) {
-		indexLabel(labelFile);
-		indexEdge(relationFile);
+	public void indexAtomicPatterns(String nqFile) {
+		InputStream ins;
+		NxParser nxp;
+		org.semanticweb.yars.nx.Node[] stmt = null;
+		
+		try {
+			ins = new FileInputStream(nqFile);
+			if (nqFile.toLowerCase().endsWith(".gz")) ins = new GZIPInputStream(ins);
+			nxp = new NxParser(ins);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		} catch (ParseException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		while (nxp.hasNext()) {
+			stmt = nxp.next();
+			
+			processStatement(stmt);
+		}
+	}
+	
+	private void processStatement(org.semanticweb.yars.nx.Node[] stmt) {
+		Set<Integer> src = new HashSet<Integer>();
+		Map<QueryGraphNode, Integer> ins; 
+		QueryGraph graph;
+		QueryGraphNode sub;
+		QueryGraphNode obj;
+		
+		int subInt = idman.addGetID(stmt[0].toString());
+		int objInt = idman.addGetID(stmt[2].toString());
+		int predInt = idman.addGetID(stmt[1].toString());
+		
+		src.add(srcman.addGetID(stmt[3].toString()));
+		
+		//Add graph of size 2
+		graph = new QueryGraph();
+		ins = new HashMap<QueryGraphNode, Integer>();
+		
+		sub = graph.addNode();
+		obj = graph.addNode();
+		graph.addEdge(sub, obj, predInt);
+		
+		ins.put(sub, subInt);
+		ins.put(obj, objInt);
+		
+		addPattern(graph, ins, src);
+		
+		//Add graph of size 1 A
+		graph = new QueryGraph();
+		ins = new HashMap<QueryGraphNode, Integer>();
+		
+		sub = graph.addNode(subInt);
+		obj = graph.addNode();
+		graph.addEdge(sub, obj, predInt);
+		
+		ins.put(sub, subInt);
+		ins.put(obj, objInt);
+		
+		addPattern(graph, ins, src);
+		
+		//Add graph of size 1 B
+		graph = new QueryGraph();
+		ins = new HashMap<QueryGraphNode, Integer>();
+		
+		sub = graph.addNode();
+		obj = graph.addNode(objInt);
+		
+		graph.addEdge(sub, obj, predInt);
+		
+		ins.put(sub, subInt);
+		ins.put(obj, objInt);
+		
+		addPattern(graph, ins, src);
+	}
+	
+	private void addPattern(QueryGraph graph, Map<QueryGraphNode, Integer> ins, Set<Integer> src) {
+        Map<QueryGraphNode, Integer> map = cnm.getMap(graph);
+        int[] tmp = new int[graph.nodeCount()];
+        
+        for (Entry<QueryGraphNode, Integer> e : ins.entrySet()) {
+                tmp[map.get(e.getKey())] = e.getValue();
+        }
+        
+        fidx.addEntry(codec.encodePattern(graph), graph.nodeCount(), tmp, src);
 	}
 
 	
 	public void close() {
 		fidx.close();
 		idman.close();
-//		lblman.close();
-	}
-	
-	private void addPattern(QueryGraph graph, Map<QueryGraphNode, Integer> ins) {
-		Map<QueryGraphNode, Integer> map = cnm.getMap(graph);
-		int[] tmp = new int[graph.nodeCount()];
-		
-		for (Entry<QueryGraphNode, Integer> e : ins.entrySet()) {
-			tmp[map.get(e.getKey())] = e.getValue();
-		}
-		
-		fidx.addEntry(codec.encodePattern(graph), graph.nodeCount(), tmp);
 	}
 	
 	public static void main(String[] args) {
 		AtomicPatternIndexService idx = new AtomicPatternIndexService(new FileConfig(args[0]));
 		
-		idx.indexAtomicPatterns(args[1], args[2]);
+		idx.indexAtomicPatterns(args[1]);
 		idx.close();
 	}
 
