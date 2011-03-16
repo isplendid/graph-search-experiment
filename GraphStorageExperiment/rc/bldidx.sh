ac32f74fec7b44c72ba18b82817a61aa5b566581
fieldfar <fieldfar@gmail.com>
2011-03-16 15:24:59 +0000

    + Scripts that build the index and make up the folder structure

---
+++ GraphStorageExperiment/rc/bldidx.sh
@@ -0,0 +1,1 @@
+java -Xmx1000m -cp bin:lib/nxparser.jar:lib/je-3.3.69.jar sjtu.apex.gse.indexer.file.AtomicPatternIndexService cfg-test data_all_spoc.nq.gz
---
+++ GraphStorageExperiment/rc/mktmpdir.sh
@@ -0,0 +1,8 @@
+mkdir tmp
+mkdir tmp/id
+mkdir tmp/lbl
+mkdir tmp/dat
+mkdir tmp/edge
+mkdir tmp/q
+mkdir tmp/src
+mkdir tmp/tmp
