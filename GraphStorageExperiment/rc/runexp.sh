68aeb8ce8f72e0f01c4834772e42c7b686ba5d74
fieldfar <fieldfar@0fcec150-9b2c-11dd-93bb-3f8bb607bf45>
2011-03-23 11:59:24 +0000

    + Experiment is run on a file by file basis

--- GraphStorageExperiment/rc/runexp.sh
+++ GraphStorageExperiment/rc/runexp.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 #
 
-query_folder=tmp/q/init
-result_folder=tmp/res/init
+query_folder=$1
+result_folder=$2
 
 if [ ! -d $result_folder ]
 then
   mkdir $result_folder
 fi
 
-for query in $query_folder/*
+for qf in $query_folder/*
 do
-  base_name=`basename $query`
-  output_file=$result_folder/$base_name
+  qf_bn=`basename $qf`
+  result_path=$result_folder/$qf_bn
 
-  echo Running $query to $output_file
-  java -Xmx400m -cp bin:lib/nxparser.jar:lib/je-3.3.69.jar:lib/lidaq_web_repos.jar sjtu.apex.gse.experiment.Experiment cfg-web $query $output_file 
+  if [ ! -d $result_path ]
+  then
+    mkdir $result_path
+  fi
+
+  for query in $qf/*
+  do
+    base_name=`basename $query`
+    output_file=$result_path/$base_name
+
+    if [ -f $output_file ]
+    then
+      continue
+    fi
+
+    echo Running $query to $output_file
+    java -Xmx4096m -cp bin:lib/nxparser.jar:lib/je-3.3.69.jar:lib/lidaq_web_repos.jar sjtu.apex.gse.experiment.Experiment cfg-web $query $output_file
+  done
 done
