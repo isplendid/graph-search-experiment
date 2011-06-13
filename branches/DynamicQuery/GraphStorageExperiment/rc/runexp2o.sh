27a8c47738dfba48f961a2b9432fa1c2f180f15f
fieldfar@gmail.com <fieldfar@gmail.com@0fcec150-9b2c-11dd-93bb-3f8bb607bf45>
2011-06-13 06:49:30 +0000

    Experiment run script with output

---
+++ branches/DynamicQuery/GraphStorageExperiment/rc/runexp2o.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+
+query_folder=$1
+result_folder=$2
+out_folder=$3
+
+if [ ! -d $result_folder ]
+then
+  mkdir $result_folder
+fi
+
+if [ ! -d $out_folder ]
+then
+  mkdir $out_folder
+fi
+
+for qf in $query_folder/*
+do
+  qf_bn=`basename $qf`
+  result_path=$result_folder/$qf_bn
+  out_path=$out_folder/$qf_bn
+
+  if [ ! -d $result_path ]
+  then
+    mkdir $result_path
+  fi
+
+  if [ ! -d $out_path ]
+  then
+    mkdir $out_path
+  fi
+
+  for query in $qf/*
+  do
+    if [ ! -f $query ]
+    then
+      continue
+    fi
+    base_name=`basename $query`
+    result_file=$result_path/$base_name
+    output_file=$out_path/$base_name
+
+    if [ -f $result_file ]
+    then
+      continue
+    fi
+
+    echo Running $query to $result_file
+    timeout -s 9 180s java -Xmx4096m -cp bin:lib/nxparser.jar:lib/je-3.3.69.jar:lib/lidaq_web_repos.jar:lib/openrdf-sesame-2.3.2-onejar.jar sjtu.apex.gse.experiment.Experiment cfg-web-1 $query $result_file $output_file > /dev/null 2>&1
+  done
+done
