6b069827abe31a3e04ffdae2eb38088cd4838f22
fieldfar <fieldfar@gmail.com>
2011-07-07 12:32:38 +0000

    + Experiment script is updated.

--- GraphStorageExperiment/rc/runexpc.sh
+++ GraphStorageExperiment/rc/runexpc.sh
@@ -3,7 +3,7 @@
 
 query_folder=$1
 result_folder=$2
-out_folder=$3
+index_no=$3
 
 if [ ! -d $result_folder ]
 then
@@ -19,7 +19,6 @@ for qf in $query_folder/*
 do
   qf_bn=`basename $qf`
   result_path=$result_folder/$qf_bn
-  out_path=$out_folder/$qf_bn
 
   if [ ! -d $result_path ]
   then
@@ -39,7 +38,6 @@ do
     fi
     base_name=`basename $query`
     result_file=$result_path/$base_name
-    output_file=$out_path/$base_name
 
     if [ -f $result_file ]
     then
@@ -47,6 +45,6 @@ do
     fi
 
     echo Running $query to $result_file
-    timeout -s 9 180s java -Xmx4096m -cp bin:lib/nxparser.jar:lib/je-3.3.69.jar:lib/lidaq_web_repos.jar:lib/openrdf-sesame-2.3.2-onejar.jar sjtu.apex.gse.exp.Experiment cfg-web-cmplx $query $result_file
+    timeout -s 9 180s java -Xmx4096m -cp bin:lib/nxparser.jar:lib/je-3.3.69.jar:lib/lidaq_web_repos.jar:lib/openrdf-sesame-2.3.2-onejar.jar sjtu.apex.gse.exp.Experiment cfg-web-cmplx$index_no $query $result_file > /dev/null 2>&1
   done
 done
